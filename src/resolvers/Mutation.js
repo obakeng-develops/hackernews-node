e69fb8128039faecdd5256cde1661bd1c7787093
Obakeng <mosadiobakeng7@gmail.com>
2021-11-22 11:58:55 +0200

    Added post resolver.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -35,6 +35,18 @@ async function login(parent, args, context, info) {
     }
 }
 
+async function post(parent, args, context, info) {
+    const { userId } = context;
+
+    return await context.prisma.link.create({
+        data: {
+            url: args.url,
+            description: args.description,
+            postedBy: { connect: { id: userId } },
+        }
+    })
+}
+
 module.exports = {
     signup,
     login,
