961406573827978d25996fb7a28cb9c52d17d417
Obakeng <mosadiobakeng7@gmail.com>
2021-11-23 13:42:21 +0200

    Added newVote subscription.

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,5 +1,9 @@
 function newLinkSubscribe(parent,args, context, info) {
-    return context.pubsub.asyncIterator("NEWLINK")
+    return context.pubsub.asyncIterator("NEW_LINK")
+}
+
+function newVoteSubscribe(parent, args, context, info) {
+    return context.pubsub.asyncIterator("NEW_VOTE")
 }
 
 const newLink = {
@@ -9,6 +13,14 @@ const newLink = {
     },
 }
 
+const newVote = {
+    subscribe: newVoteSubscribe,
+    resolve: payload => {
+        return payload
+    },
+}
+
 module.exports = {
     newLink,
+    newVote,
 }
